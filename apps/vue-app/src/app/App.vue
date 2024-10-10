54ebf3b8546b1503e1f1f8ae3e25669d5476c453
Jorge Orozco <jorge.a.orozco@accenture.com>
2024-10-10 11:31:48 -0600

    Activities Session 2

--- apps/vue-app/src/app/App.vue
+++ apps/vue-app/src/app/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <PostView />
+  <PostDetailView />
   <!-- <PostDetailView /> -->
 </template>
 
 <script>
+import PostDetailView from './modules/posts/views/PostDetailView.vue';
 import PostView from './modules/posts/views/PostView.vue';
-/* import PostDetailView from './modules/posts/views/PostDetailView.vue'; */
+import CategoryView from './modules/categories/views/CategoryView.vue';
+import SignUpView from './modules/auth/views/SignUpView.vue';
+import LoginView from './modules/auth/views/LoginView.vue';
 
 export default {
   components: {
-    PostView
-    /* PostDetailView */
+    PostView,
+    CategoryView,
+    SignUpView,
+    LoginView,
+    PostDetailView
   }
 };
 </script>
